a646c352d1491066ee47e8b735c140b4046d5d33
Bryce Kunz <bkunz9@huskers.unl.edu>
2025-08-28 08:26:35 -0500

    Fixed Standard Library Bug

--- hello.c
+++ hello.c
@@ -4,6 +4,9 @@
 
  int main(int argc, char **argv) {
 
+    #include <stdlib.h>
+    #include <stdio.h>
+
     printf("Hello, I'm Bryce Kunz!\n")
     printf("My Major is Computer Engineering!\n")
 
